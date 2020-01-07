25fb40ee1c63a27cb9ad71d042e7200e5e991b7d
ronnie <rogwang123@gmail.com>
2020-01-07 17:17:13 +0300

    Drawn lines on original image

--- section5_Finding_lanes/lanes.py
+++ section5_Finding_lanes/lanes.py
@@ -1,8 +1,8 @@
-#computer vision library
+#import libraries
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-#import image
+
 
 #grayscale
 #gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
@@ -21,6 +21,14 @@ def canny(image):
     canny = cv2.Canny(blur, 50,150)
     return canny
 
+def display_lines(image,lines):
+    line_image = np.zeros_like(image)
+    if lines is not None: 
+        for line in lines:  #each line is 2D array (x1,y1,x2,y2)
+            x1,y1,x2,y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1),(x2,y2),(255,0,0),10)#(255,0,0 - bgr)
+    return line_image
+
 def region_of_interest(image):
     height = image.shape[0] #image height
     triangle = np.array([[(200,height), (1100,height), (550,250)]])#fillpoly fills with more than one polygon
@@ -29,15 +37,22 @@ def region_of_interest(image):
     masked_image = cv2.bitwise_and(mask,image)
     return masked_image
 
+#import image
 image = cv2.imread('test_image.jpg')
-lane_image = np.copy(image)
+lane_image = np.copy(image) #to avoid changing original image
 
+#region of interest
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
 
-#this ensures the priginal image is not tampered with
+#hough transform
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40 , maxLineGap=5)#(2 & pi/180 are definining grid size)
+line_image = display_lines(lane_image,lines)
 
+#combine line image with original image
+
+combined_image = cv2.addWeighted(lane_image, 0.8, line_image, 1,1)#weights make on image more vivid in this case lines
 #show image
-cv2.imshow('result',cropped_image)
+cv2.imshow('result',combined_image)
 cv2.waitKey(0) #waitkey 0 keeps window infinitely open till we press any key
 
